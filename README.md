6071e184efe62eddb76c443878a6d10e2f7cfec1
Kindzerske, Matthew <matthew.kindzerske@accenture.com>
2019-06-25 16:22:37 +0100

    empty express page added

--- README.md
+++ README.md
@@ -6,6 +6,11 @@ A quick web-app to help accelerate.
 
 ## Backlog
 
-- Better Readme
 - first nodejs page
 - first form
+- submission of form content, and executing the module
+- returning the info
+- rendering errors first
+- rendering straight print-out of valid results
+- more fancy rendering
+- Better Readme
--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+var express = require('express');
+var app = express();
+
 var cidrizer = require('./modules/cidrizer.js');
+
+const PORT_NUMBER = 8080;
 
 // cidrizer.init();
 
@@ -16,3 +21,18 @@ results = cidrizer.doLowestBlocking('10.82.208.0/20',
     ['10.82.217.128/27', '10.82.218.0/27', '10.82.218.32/27']);
 
 console.log(results);
+
+
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page 
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+app.listen(PORT_NUMBER);
+console.log(`${PORT_NUMBER} is the magic port`);
